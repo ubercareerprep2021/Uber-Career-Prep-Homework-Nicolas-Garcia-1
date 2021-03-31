81a7b4264a75b6b488861924bff8440c1ed528ce
nico <nicolas17g@gmail.com>
2021-03-31 11:49:24 -0500

    Part 1 done

--- Assignment -1/Part1.py
+++ Assignment -1/Part1.py
@@ -5,23 +5,24 @@ Created on Tue Mar 30 22:16:12 2021
 @author: nicol
 """
 
-#go thru first string
-#check against second string for each letter
-#remove letter once found
+#method 1
 
 #test cases: 
     
 #1   s1: asdf, s2: fsda == true
 #2   s1: asdf, s2: fsa == false
 #3   s1: asdf, s2: fsax == false
-
+#4   s1: asda, s2: dasa == true
+#5   s1: aaaa, s2: sadx == false
+#6   s1: sadx, s2: aaaa == false
 def isStringPermutation (s1: str, s2: str):
     if len(s1) != len(s2):
         return False
     
     for i in s1:
-        if i not in s2:
-            return False
+        for j in s2:
+            if i not in s2 or j not in s1:
+                return False
         
     return True    
         
@@ -29,5 +30,9 @@ def isStringPermutation (s1: str, s2: str):
 print(isStringPermutation('asdf', 'fsda'))
 print(isStringPermutation('asdf', 'fsa'))
 print(isStringPermutation('asdf', 'fsax'))
-print(isStringPermutation('asda', 'dasa'))            
-    
+print(isStringPermutation('asda', 'dasa'))
+print(isStringPermutation('aaaa', 'sadx'))             
+print(isStringPermutation('sadx', 'aaaa'))    
+
+#method 2
+
