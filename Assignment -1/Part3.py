900a96cf17de37cbd4a663613b154ea9beb157fc
nico <nicolas17g@gmail.com>
2021-04-15 21:30:13 -0500

    part 3 done

--- Assignment -1/Part3.py
+++ Assignment -1/Part3.py
@@ -5,22 +5,74 @@ Created on Wed Apr 14 19:00:14 2021
 @author: nicol
 """
 
-#don't know how to implement it into class or how to use __int__   
-list1 = [1, 2, 3]
 
-def Push(integer: int):
-    list1.insert(0, integer)
+class Stack:
     
-def Pop():
-    list1.pop(0)
-    
-def Top():
-    return list1[0]
-
-def isEmpty():
-    return len(list1) == 0
+    def __init__(self):
+        self.List = []
         
-def Size():
-    return len(list1)
+    def Push(self, integer: int):
+        self.integer = integer
+        self.List.insert(0, integer)
     
-print(Size())
+    def Pop(self):
+        self.List.pop(0)
+    
+    def Top(self):
+        return self.List[0]
+
+    def isEmpty(self):
+        return len(self.List) == 0
+        
+    def Size(self):
+        return len(self.List)
+    
+s = Stack()
+
+s.Push(1)
+s.Push(2)
+s.Push(5)
+print(s.List) #[5, 2, 1]
+s.Pop()
+print(s.List) #[2, 1]
+print(s.Top()) #2
+print(s.isEmpty()) #False
+print(s.Size()) #2
+print()
+
+class Queue:
+    
+    def __init__(self):
+        self.List = []
+        
+    def Enqueue(self, item):
+        self.item = item
+        self.List.append(item)
+    
+    def Dequeue(self):
+        self.List.remove(self.List[0])
+    
+    def Rear(self):
+        return self.List[-1]
+    
+    def Front(self):
+        return self.List[0]
+    
+    def Size(self):
+        return len(self.List)
+    
+    def isEmpty(self):
+        return len(self.List) == 0
+    
+q = Queue()
+q.Enqueue('item 1')
+q.Enqueue('item 2')
+q.Enqueue('item 3')
+q.Enqueue(4)
+print(q.List) #["item 1", "item 2", "item 3", 4]
+q.Dequeue()
+print(q.List)#["item 2", "item 3", 4]
+print(q.Rear()) #4
+print(q.Front()) #"item 2"
+print(q.Size()) #3
+print(q.isEmpty()) #False
